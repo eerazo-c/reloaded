a474fbb3153af28cac3049a430b296d72998080d
Elizabeth <eerazo-c@eerazopc>
2024-09-12 22:26:00 +0200

    avance

--- ex06/ft_print_alphabet.c
+++ ex06/ft_print_alphabet.c
@@ -9,11 +9,25 @@
 /*   Updated: 2024/09/11 18:48:40 by elerazo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
 void	ft_print_alphabet(void)
 {
 	char	c;
-	
+	int	i;
+
+	c = 'a';
 	i = 0;
-	while (c < a )
+	while (i < 26)
+	{
+		write(1, &c, 1);
+		c++;
+		i++;
+	}
+}
+
+int main ()
+{
+	ft_print_alphabet();
+	return (0);
 }
---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void	ft_print_numbers(void)
+{
+	char	num;
+	int	i;
+
+	num = '0';		i = 0;
+	while (i <= 9)
+	{
+		write(1, &num, 1);
+		num++;
+		i++;
+	}
+}
+
+int main ()
+{
+	ft_print_numbers();
+}
